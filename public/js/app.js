5441a66daea0b6ac33024d88905176f2ff1b7a8e
Mafaz Zafar <mzafar@ahslink.com>
2016-12-30 08:59:37 -0600

    Added custome message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,10 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
+
+//Following code listens for custome event 'message' 
+//and prints the content of the message object received.
+socket.on('message', function(message) {
+	console.log('New message');
+	console.log (message.text);
+});
--- server.js
+++ server.js
@@ -6,8 +6,26 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!')
+
+	socket.on('message', function(message) {
+		console.log('Message received:' + message.text);
+
+		//Now send the received message to all clients attached 
+		//to the server excpet the sender. Can use io.emit 
+		//if we need to send it to all including the sender.
+		socket.broadcast.emit('message', message);
+	})
+
+	//Now we will use socket to emit a custom event.
+	//socket.emit take two args.
+	//first the event name we used custom event name 'message',
+	//second the data to send. We used object with property text.
+	
+	socket.emit('message', {
+		text: 'Welcome to the chat application!'
+	});
 });
 
 http.listen(PORT, function () {
