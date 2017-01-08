bdef37d771d4ecf894976ecfca0799aeb452cf06
Mafaz Zafar <mzafar@ahslink.com>
2017-01-08 17:05:24 -0600

    Added @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,35 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+//Function getCurrentUsers which determines the list of users in the room.
+function getCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	//Array to hold user names
+	var users = [];
+
+	//Check if the info has something
+	if (typeof info === 'undefined') {
+		return;
+	} 
+
+	//Object.keys(clientInfo) returns the array of all the keys in the clientInfo
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if(info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+
+	});
+
+	socket.emit('message', {
+			name: 'System',
+			timestamp: moment().valueOf(),
+			//Note: The join function on users arrays will conctinate all users the ', ' arg to join will be betwen names
+			text: 'Current Users:' + users.join(', ')
+		});
+}
+
 io.on('connection', function (socket) {
 	var timestamp = moment().format('x');
 	console.log(timestamp + ': User connected via socket.io!')
@@ -53,7 +82,13 @@ io.on('connection', function (socket) {
 	socket.on('message', function(message) {
 		console.log('Message received:' + message.text);
 
-		//Add the time to the message object. 
+		//Check if the sent message is a command @currentUsers
+		if(message.text === '@currentUsers') {
+			//Call the function which prints the list of users in the same room.
+			getCurrentUsers(socket);
+
+		} else {
+			//Add the time to the message object. 
 		//Called moment as a function to get the current timestamp and then used vauleOf to get
 		//the javascript version of time i.e. in milli seconds since Jan 1, 1970
 		message.timestamp = moment().valueOf();
@@ -70,12 +105,16 @@ io.on('connection', function (socket) {
 		//To emit message to all users of a specific room we use the following.
 		//We used cilientInfo of the current socket to access the room we want to send the message to.
 		io.to(clientInfo[socket.id].room).emit('message', message);
+
+		}
+
+		
 	})
 
 	//Now we will use socket to emit a custom event.
 	//socket.emit take two args.
 	//first the event name we used custom event name 'message',
-	//second the data to send. We used object with property text.
+	//second the data to send. We used object with properties name, timestamp, text.
 	
 	socket.emit('message', {
 		name: 'System',
